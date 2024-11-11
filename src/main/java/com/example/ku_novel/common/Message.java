c20292e6e805f4e443b510a516964703fd9c1284
우승식 <intelryzen@naver.com>
2024-11-11 17:56:45 +0900

    feat: nickname, password

--- src/main/java/com/example/ku_novel/common/Message.java
+++ src/main/java/com/example/ku_novel/common/Message.java
@@ -2,6 +2,7 @@ package com.example.ku_novel.common;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
 import java.time.Instant;
 
 public class Message {
@@ -11,16 +12,20 @@ public class Message {
     private String receiver;
     private String content;
     private String timestamp;
+    private String password;
+    private String nickname;
 
     // GSON 라이브러리 사용을 위해 빈 생성자가 필요함
-    public Message() {}
+    public Message() {
+    }
 
-    public Message(MessageType type, String sender, String receiver, String content) {
+    public Message(MessageType type, String sender, String receiver, String content, String password, String nickname) {
         this.type = type;
         this.sender = sender;
         this.receiver = receiver;
         this.content = content;
-
+        this.password = password;
+        this.nickname = nickname;
         this.timestamp = Instant.now().toString();
     }
 
@@ -44,6 +49,17 @@ public class Message {
         return this;
     }
 
+    public Message setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public Message setNickname(String nickname) {
+        this.nickname = nickname;
+        return this;
+    }
+
+
     // Getters
     public MessageType getType() {
         return type;
@@ -65,6 +81,14 @@ public class Message {
         return timestamp;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
     public String toJson() {
         Gson gson = new GsonBuilder().create();
         return gson.toJson(this);
@@ -74,7 +98,7 @@ public class Message {
     public String toString() {
         return toJson().toString();
     }
-    
+
     public static Message fromJson(String json) {
         Gson gson = new GsonBuilder().create();
         return gson.fromJson(json, Message.class);
