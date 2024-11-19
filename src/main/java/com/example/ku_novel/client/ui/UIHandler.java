26c14eae62dab805ec85e132e0e8396a1337e9d6
light11014 <83333513+light11014@users.noreply.github.com>
2024-11-19 09:55:58 +0900

    MyPage 수정

--- src/main/java/com/example/ku_novel/client/ui/UIHandler.java
+++ src/main/java/com/example/ku_novel/client/ui/UIHandler.java
@@ -6,6 +6,8 @@ import com.example.ku_novel.common.Message;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.concurrent.ConcurrentLinkedQueue;
