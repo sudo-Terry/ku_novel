509800d9089a558dad1c4870d41b20c0683ff136
Seungsik Woo <intelryzen@naver.com>
2024-11-22 17:18:10 +0900

    fix: import 제거

--- src/main/java/com/example/ku_novel/client/connection/ClientListenerThread.java
+++ src/main/java/com/example/ku_novel/client/connection/ClientListenerThread.java
@@ -1,6 +1,5 @@
 package com.example.ku_novel.client.connection;
 
-import com.example.ku_novel.client.model.ClientDataModel;
 import com.example.ku_novel.client.ui.HomeUI;
 import com.example.ku_novel.client.ui.UIHandler;
 import com.example.ku_novel.common.Message;
